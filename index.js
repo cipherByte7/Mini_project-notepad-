cddd4193124802bcb251276dead6006fb30a225b
cipherByte7 <adityaschitale0816@gmail.com>
2026-06-18 00:24:54 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get("/", function(req, res){
+//     res.send("Welcome");
+// });
+
+app.get("/", function(req, res){
+    res.render('index.ejs')
+});
+
+
+app.listen(3000, function(){
+    console.log("its running");
+})
